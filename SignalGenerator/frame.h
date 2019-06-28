bd2ec33754498a1eab42603213f3090410582654
Egor Ryazantsev <egorryazancev1908@mail.ru>
2019-06-28 16:58:23 +0700

    Добавлена структура Frame

---
+++ SignalGenerator/frame.h
@@ -0,0 +1,25 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+#include <QString>
+#include <QVector>
+#include <QVariant>
+
+struct Frame {
+    quint8 frameNumber;
+    QString channelName;
+    QString xMeasure;
+    QString yMeasure;
+    float divisionXValue;
+    float divisionYValue;
+    qint32 offsetX;
+    qint32 time;
+
+    bool isComplex;
+    bool isFloat;
+    quint8 pointSize;
+
+    QVector <QVariant> points;
+};
+
+#endif // FRAME_H
